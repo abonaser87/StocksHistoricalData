035ffcfadde10021e82447f33c7889d0eeaa17a9
abonaser87 <abonaser.87@gmail.com>
2017-12-14 13:12:46 +0300

    Display a list of the companies

--- freport/dbmaker/views.py
+++ freport/dbmaker/views.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+from models import Company
+
 
 def index(request):
-    return render(request,'dbmaker/home.html')
+    template = loader.get_template('dbmaker/home.html')
+    context = {
+        'companies': Company.objects.all()
+    }
+    return HttpResponse(template.render(context, request))
